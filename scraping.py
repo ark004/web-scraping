fbc6f8d0eb71132bafc5ef55f6fb4b2792e65b56
unknown <anooprkrishnaf55@gmail.com>
2022-10-13 00:03:41 +0530

    web scraping

---
+++ scraping.py
@@ -0,0 +1,28 @@
+from ast import keyword
+import requests
+from bs4 import BeautifulSoup
+website_url = "https://infopark.in/companies/job-search"
+
+keywords = ["python","fresher"]
+out_file=open("jobs.txt","w")
+
+res = requests.get(website_url)
+soup = BeautifulSoup(res.text,'lxml')
+jobs = soup.find_all("div",{"class":"row company-list joblist"})
+
+for job in jobs:
+    title_element=job.find("a")
+    title=title_element.text
+    link=title_element["href"]
+    company_nm= soup.find("div",{"class":"jobs-comp-name"}).text
+    last_date = soup.find("div",{"class":"job-date"}).text
+
+    if any(word.lower() in title.lower()  for word in keywords):
+
+       # print(title,link,company_nm,last_date)
+
+        out_file.write(title + " " + company_nm + " " + last_date +"\n"+ link +"\n\n")
+
+
+    
+  
